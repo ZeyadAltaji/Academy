2c167885b8addca739db9b246f5fe7ba2d152bf0
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-10-06 20:35:49 +0300

    add categoery

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Academy.Models
+{
+    public class CategoryModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public string ParentName { get; set; }
+    }
+}
