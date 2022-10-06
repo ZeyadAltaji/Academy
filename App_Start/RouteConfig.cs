5c0ef5b40c1b6655045dd0c650e7be8ff3c2cdfb
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-10-06 18:49:20 +0300

    commit

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Academy
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }, new string[] { "Academy.Controllers" }
             );
         }
     }
