921a09d22b1d54328ff9efcf17670f610d85386f
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-10-06 17:31:35 +0300

    created page home and addetion to Areas <created by Eng Zeyad altaji>

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,9 +15,7 @@ namespace Academy
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+             RouteConfig.RegisterRoutes(RouteTable.Routes);
+         }
     }
 }
