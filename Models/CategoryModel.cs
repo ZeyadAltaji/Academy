38013d2d2df8923b5a9a99f68c0a4e69b678b279
Zeyad Altaji <ziadaltaji130@gmail.com>
2022-10-06 23:10:42 +0300

    vallidetion to model

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Academy.Models
     public class CategoryModel
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Category Name Is Required")]
+        [StringLength(200 ,MinimumLength=10 ,ErrorMessage ="Category name be should be between 10 and 200")]
         public string Name { get; set; }
         public int? ParentId { get; set; }
         public string ParentName { get; set; }
